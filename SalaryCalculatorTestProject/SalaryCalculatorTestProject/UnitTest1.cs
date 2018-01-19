c332618e13f5a364e97b96c245392da1ab249fb9
Neous1 <Neous1@users.noreply.github.com>
2018-01-19 15:11:00 -0500

    initial commit

---
+++ SalaryCalculatorTestProject/SalaryCalculatorTestProject/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SalaryCalculatorTestProject
+{
+    /***
+    To get hourly, divide annnual salary by 2080
+    $100,006.40/2080 = $48.08 hr
+
+    Toget annual, multiply hourly by 2080
+    $48.08 * 2080
+    
+    */
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
