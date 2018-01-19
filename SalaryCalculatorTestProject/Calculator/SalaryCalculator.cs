1273597c1ae7305032662a6872f24a1ebdf33c30
Neous1 <Neous1@users.noreply.github.com>
2018-01-19 16:13:41 -0500

    added a test method for Hourly Wage added method in SalaryCalculator to return hourly wage

--- SalaryCalculatorTestProject/Calculator/SalaryCalculator.cs
+++ SalaryCalculatorTestProject/Calculator/SalaryCalculator.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace Calculator
 {
     public class SalaryCalculator
-
     {
+        //move const HoursInYear out of the getAnnualSalary to make it global
         const int hoursInYear = 2080;
         public decimal GetAnnualSalary(decimal hourlyWage)
         {
@@ -16,6 +16,12 @@ namespace Calculator
             decimal annualSalary = hourlyWage * hoursInYear;
 
             return annualSalary;
+        }
+
+        public decimal GetHourlyWage(int annualSalary)
+        {
+
+            return annualSalary / hoursInYear;
         }
     }
 }
