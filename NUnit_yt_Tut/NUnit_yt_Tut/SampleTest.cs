e2b0d02d76b79a57d644f8e6c97143ee3aaa91b9
Neous1 <Neous1@users.noreply.github.com>
2018-01-20 05:24:39 -0500

    Add sampletest

---
+++ NUnit_yt_Tut/NUnit_yt_Tut/SampleTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_yt_Tut
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public void TestApp()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
