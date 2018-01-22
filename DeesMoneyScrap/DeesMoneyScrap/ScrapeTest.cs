eaa71a3fa7c00dda2513ee74876190375fd22452
Neous1 <Neous1@users.noreply.github.com>
2018-01-21 21:45:36 -0500

    add NUnit framework Add test for login

---
+++ DeesMoneyScrap/DeesMoneyScrap/ScrapeTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeesMoneyScrap
+{
+    [TestFixture]
+    public class ScrapeTest
+    {
+        [Test]
+        public void testLogin()
+        {
+            Console.WriteLine("login to yahoo");
+        }
+    }
+}
