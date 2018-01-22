a73d2d681e04d47206246314bd5ca17641fc1c97
Neous1 <Neous1@users.noreply.github.com>
2018-01-21 23:14:47 -0500

    Added code to login to yahoo

--- DeesMoneyScrap/DeesMoneyScrap/ScrapeTest.cs
+++ DeesMoneyScrap/DeesMoneyScrap/ScrapeTest.cs
@@ -13,7 +13,20 @@ namespace DeesMoneyScrap
         [Test]
         public void testLogin()
         {
-            Console.WriteLine("login to yahoo");
+            //navigate to the url
+            Drivers.Driver.Navigate()
+                .GoToUrl(
+                    "https://login.yahoo.com/?.src=fpctx&.intl=us&.lang=en-US&authMechanism=primary&yid=&done=https%3A%2F%2Fwww.yahoo.com%2F&eid=100&add=1");
+
+            //pass username and password 
+            var logMeIn = new Login("jayd9817", "ICG9817#");
+            logMeIn.LogIn();
+            //Console.WriteLine("login to yahoo");
+        }
+        [Test]
+        public void testAccesstoPortfolio()
+        {
+            
         }
     }
 }
